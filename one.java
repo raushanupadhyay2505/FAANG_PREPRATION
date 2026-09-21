00ca18f6bf1c611816b3e97bfa60222d78fe2230
Raushanupadhyay <raushan.upadhayays@gmail.com>
2026-09-21 22:50:42 +0530

    first commit

---
+++ one.java
@@ -0,0 +1,10 @@
+package revision;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("# # # #");
+        System.out.println("# # #");
+        System.out.println("# #");
+        System.out.println("#");
+    }
+}
